73ddadc935e2ecff5ee4c012bb222f3a35e50583
wildanar29 <90098635+wildanar29@users.noreply.github.com>
2023-09-27 15:42:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,65 +1,27 @@
+# Import library Kivy
+import kivy
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
 from kivy.uix.button import Button
-from kivy.properties import StringProperty
-# import paho.mqtt.client as paho
-from datetime import datetime
 
-# broker = "broker.hivemq.com"
-# port = 1883
-pesan = ""
-date = datetime.now()
-current_date = date.strftime("%D")
-# def on_message(client, userdata, message):
-#     msg = str(message.payload.decode("utf-8"))
-#     t = str(message.topic)
-#     if (t == "toko_solusa"):
-#         global pesan
-#         pesan = str(msg)
-#         print(pesan)
+# Inisialisasi Kivy (opsional)
+kivy.require('1.11.1')
 
-class MyApp(App):
-    text_to_change = StringProperty("Initial Text")
-
-    def change_text(self):
-        self.text_to_change = pesan
-
+# Aplikasi Kivy yang sederhana
+class SimpleKivyApp(App):
+    
+    # Fungsi yang akan dipanggil saat aplikasi dimulai
     def build(self):
-        # Create the root widget, which will be a BoxLayout
-        root = BoxLayout(orientation='vertical')
+        # Membuat tombol
+        button = Button(text='Klik Saya!', size_hint=(.5, .5), pos=(200, 200))
+        # Menambahkan fungsi yang akan dipanggil saat tombol diklik
+        button.bind(on_press=self.on_button_click)
+        # Mengembalikan tampilan utama
+        return button
 
-        # Create the label widget
-        label = Label(text=f'Omset {current_date}',
-                      font_size='24sp',  # Set the font size to 24
-                      color=(0, 1, 0, 1))  # Set the text color to green (RGBA format)
+    # Fungsi yang akan dipanggil saat tombol diklik
+    def on_button_click(self, instance):
+        print("Tombol diklik!")
 
-        # Add the label widget to the root widget
-        root.add_widget(label)
-
-        # Create the text label with the StringProperty value
-        self.text_label = Label(text=self.text_to_change,
-                                font_size='20sp',
-                                halign='center')  # Center-align the text
-        root.add_widget(self.text_label)
-
-        # Create a button to change the text dynamically
-        button = Button(text="Update",
-                        size_hint=(None, None),
-                        size=(150, 50),
-                        pos_hint={'center_x': 0.5})
-        button.bind(on_press=lambda instance: self.change_text())
-        root.add_widget(button)
-
-        return root
-
-    def on_text_to_change(self, instance, value):
-        self.text_label.text = value
-
+# Jalankan aplikasi Kivy
 if __name__ == '__main__':
-    # client= paho.Client("GUI")
-    # client.on_message=on_message
-    # client.connect(broker,port)#connect
-    # client.loop_start()
-    # client.subscribe('toko_solusa')
-    MyApp().run()
+    SimpleKivyApp().run()
